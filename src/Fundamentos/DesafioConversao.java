fa6d86a3c5ae1f89b96272afba1e355488c4cba5
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-08 11:47:59 -0300

    feito desafio

--- src/Fundamentos/DesafioConversao.java
+++ src/Fundamentos/DesafioConversao.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class DesafioConversao {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.printf("Digite o seu nome: ");
-		final var nome = input.nextLine();
-		System.out.printf("O nome é %s", nome);
+		System.out.printf("Digite um valor: ");
+		final var a = Double.parseDouble(input.nextLine());
+		System.out.printf("Digite um outro valor: ");
+		final var b = Double.parseDouble(input.nextLine());
+		System.out.printf("A soma dos valores é %.0f", a + b);
 		input.close();
 	}
 }
