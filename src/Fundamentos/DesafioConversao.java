d01a99837eaacbdd7d804ee57b05b01efd1b4824
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-09 15:31:47 -0300

    Lero Lero

--- src/Fundamentos/DesafioConversao.java
+++ src/Fundamentos/DesafioConversao.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class DesafioConversao {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.printf("Digite um valor: ");
-		final var a = Double.parseDouble(input.nextLine());
-		System.out.printf("Digite um outro valor: ");
-		final var b = Double.parseDouble(input.nextLine());
-		System.out.printf("A soma dos valores é %.0f", a + b);
+		System.out.print("Digite um valor: ");
+		final var a = convertStringToDouble(input.next());
+		System.out.print("Digite um outro valor: ");
+		final var b = convertStringToDouble(input.next());
+		System.out.print("Digite outro ainda: ");
+		final var c = convertStringToDouble(input.next());
+		System.out.printf("A soma dos valores é %.2f e a média é %.2f", a + b + c, (a + b + c)/3);
 		input.close();
+	}
+	static double convertStringToDouble(String value) {
+		return Double.parseDouble(value.replace(",", "."));
 	}
 }
