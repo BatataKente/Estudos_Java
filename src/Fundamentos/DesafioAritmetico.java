3008f7c2fd5d98511bac7866b6cfd766e87ce3de
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-10 16:56:40 -0300

    fazendo da forma ensinada na aula

--- src/Fundamentos/DesafioAritmetico.java
+++ src/Fundamentos/DesafioAritmetico.java
@@ -2,7 +2,7 @@ package Fundamentos;
 
 public class DesafioAritmetico {
 	public static void main(String[] args) {
-		System.out.printf("Resultado -> %.0f", stepCalculate());
+		System.out.printf("Resultado -> %.0f", calculate());
 	}
 	static double stepCalculate() {
 		double step = 3 + 2; 
@@ -13,5 +13,16 @@ public class DesafioAritmetico {
 		step = Math.pow(step, 3);
 		step /= Math.pow(10, 3);
 		return step;
+	}
+	static double calculate() {
+		double numA = Math.pow(6 * (3 + 2), 2);
+		double denA = 3 * 2;
+		double superA = numA/denA;
+		double numB = (1 - 5) * (2 - 7);
+		double denB = 2;
+		double superB = Math.pow(numB/denB, 2);
+		double num = Math.pow(superA - superB, 3);
+		double den = Math.pow(10, 3);
+		return num/den;
 	}
 }
