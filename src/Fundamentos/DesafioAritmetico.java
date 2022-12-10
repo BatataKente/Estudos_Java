1ea391c1994d1d5aa7e4228efa3fe36aab42ee70
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-10 16:35:40 -0300

    feito em steps

--- src/Fundamentos/DesafioAritmetico.java
+++ src/Fundamentos/DesafioAritmetico.java
@@ -3,6 +3,24 @@ package Fundamentos;
 public class DesafioAritmetico {
 	public static void main(String[] args) {
 		var a = Math.pow((((Math.pow((6*(3+2)), 2))/(3*2))-(Math.pow(((1-5)*(2-7)/2), 2))), 3)/Math.pow(10, 3);
-		System.out.printf("Resultado -> %.0f", a);
+		System.out.printf(
+				"\nResultado -> %.0f", stepCalculate()
+		);
+	}
+	static double stepCalculate() {
+		double step = 3 + 2; 
+		System.out.printf("%.0f\n", step);
+		step *= 6;
+		System.out.printf("%.0f\n", step);
+		step = Math.pow(step, 2);
+		System.out.printf("%.0f\n", step);
+		step /= (3*2);
+		System.out.printf("%.0f\n", step);
+		step -= Math.pow((1 - 5)*(2 - 7)/2, 2);
+		System.out.printf("%.0f\n", step);
+		step = Math.pow(step, 3);
+		System.out.printf("%.0f\n", step);
+		step /= Math.pow(10, 3);
+		return step;
 	}
 }
