771f101e416aae8c5f389de8fb116afeebb964dc
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-16 15:24:15 -0300

    lero lero

--- src/Controle/Lacos/DoWhile.java
+++ src/Controle/Lacos/DoWhile.java
@@ -1,5 +1,19 @@
 package Controle.Lacos;
 
-public class DoWhile {
+import java.util.Scanner;
 
+public class DoWhile {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String phrase = null;
+		do {
+			System.out.print(
+					"Você precisa falar as palavras mágicas..."
+			);
+			System.out.print("\nQuer sair? ");
+			phrase = input.nextLine();
+		} while (!phrase.equalsIgnoreCase("por favor"));
+		System.out.print("Obrigado!");
+		input.close();
+	}
 }
