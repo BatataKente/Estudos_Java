05529e0598164e181b4be69eed77c5f991ea2303
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-26 12:41:04 -0300

    Lero LEro

--- src/Classe/Desafio/Jantar.java
+++ src/Classe/Desafio/Jantar.java
@@ -3,10 +3,19 @@ package Classe.Desafio;
 public class Jantar {
 	public static void main(String[] args) {
 		final var josicleison  = new Pessoa("Josicleison", 70);
-		final var sanduicheDePresunto = new Comida("Sanduiche de presunto", 2);
-		final var pudim = new Comida("Pudim", 0.5);
-		josicleison.comer(sanduicheDePresunto);
-		josicleison.comer(pudim);
-		System.out.print(josicleison.peso);
+		final var josicleide  = new Pessoa("Josicleide", 60);
+		final Comida[] comidas = {
+				new Comida("Sanduiche de presunto", 2),
+				new Comida("Pudim", 0.5)
+		};
+		servir(comidas, josicleison);
+		servir(comidas, josicleide);
+		System.out.println(josicleison.peso);
+		System.out.print(josicleide.peso);
+	}
+	static void servir(final Comida[] comidas, final Pessoa pessoa) {
+		for (Comida comida : comidas) {
+			pessoa.comer(comida);
+		}
 	}
 }
