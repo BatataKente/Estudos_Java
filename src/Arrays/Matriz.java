c4ea4289ae1b9be83109ad5b34afc1c66ed63672
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-28 14:03:55 -0300

    Estudando sobre arrays

--- src/Arrays/Matriz.java
+++ src/Arrays/Matriz.java
@@ -1,30 +1,54 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Matriz {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		mySolution(input);
-		input.close();		
+		solution(input);
+		input.close();
+	}
+	static void solution(Scanner input) {
+		System.out.print("Quantos alunos? ");
+		var numberOfAlunos = input.nextInt();
+		System.out.print("Quantas notas por aluno? ");
+		var numberOfNotas = input.nextInt();
+		double[][] notasDaTurma = new double[numberOfAlunos][numberOfNotas];
+		double total = 0;
+		for (int student = 0; student < notasDaTurma.length; student++) {
+			System.out.println();
+			for (int note = 0; note < notasDaTurma[student].length; note++) {
+				System.out.printf("Informe a nota %d do aluno %d: ", note + 1, student + 1);
+				notasDaTurma[student][note] = input.nextDouble();
+				total += notasDaTurma[student][note];
+			}
+		}
+		double media = total/(numberOfAlunos*numberOfNotas);
+		System.out.printf("\nA media da turma é %.2f\nNotas da turma: ", media);
+		for (double[] notasDoAluno : notasDaTurma) {
+			System.out.print(Arrays.toString(notasDoAluno) + " ");
+		}
 	}
 	static void mySolution(Scanner input) {
-		var numberOfNotes = 0;
-		var numberOfStudents = 0;
 		System.out.print("Quantos alunos tem na turma? ");
-		numberOfStudents = input.nextInt();
+		var numberOfStudents = input.nextInt();
 		System.out.print("Quantas notas você deseja informar? ");
-		numberOfNotes = input.nextInt();
+		var numberOfNotes = input.nextInt();
 		double[][] notas = new double[numberOfStudents][numberOfNotes];
+		double total = 0;
 		for (int studentId = 0; studentId < notas.length; studentId++) {
-			double total = 0; 
+			double  studentTotal = 0; 
 			System.out.print("\nAluno " + (studentId + 1) + "\n");
 			for (int counter = 0; counter < notas[studentId].length; counter++) {
 				System.out.print("\tDigite a nota " + (counter + 1) + ": ");
 				notas[studentId][counter] = input.nextDouble();
-				total += notas[studentId][counter];
+				studentTotal += notas[studentId][counter];
 			}
-			System.out.printf("\nA média do aluno é: %.2f\n", total/notas.length);
+			var media = studentTotal/notas[studentId].length;
+			total += media;
+			System.out.printf("\nA média do aluno é: %.2f\n", media);
 		}
+		System.out.printf("\nA média da turma é: %.2f\n", total/notas.length);
 	}
 }
