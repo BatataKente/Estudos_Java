e58e1fb04ed3a6f0ce9d3368f955e2cf5cd5f94d
Audioteca Ancap <danielpotoco@gmail.com>
2022-12-30 13:53:38 -0300

    Feitos novos estudos

---
+++ src/Collections/Hash.java
@@ -0,0 +1,15 @@
+package Collections;
+
+import java.util.HashSet;
+import Collections.Listas.User;
+
+public class Hash {
+	public static void main(String[] args) {
+		HashSet<User> users = new HashSet<User>();
+		users.add(new User("Josicleison"));
+		users.add(new User("Josicleide"));
+		users.add(new User("Ana"));
+		boolean test = users.contains(new User("Josicleison"));
+		System.out.println(test);
+	}
+}
---
+++ src/Lero/Compra/Item.java
@@ -0,0 +1,13 @@
+package Lero.Compra;
+
+public class Item {
+	String nome;
+	int quantidade;
+	double preco;
+	Compra compra;
+	public Item(String nome, int quantidade, double preco) {
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+}
