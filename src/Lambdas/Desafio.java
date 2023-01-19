9be7401ac65b8fa3aa95e67d80f3d17323fb053f
Audioteca Ancap <danielpotoco@gmail.com>
2023-01-19 17:29:13 -0300

    studed lambda

--- src/Lambdas/Desafio.java
+++ src/Lambdas/Desafio.java
@@ -6,20 +6,47 @@ import java.util.function.UnaryOperator;
 
 public class Desafio {
 	public static void main(String[] args) {
-		teste();
+//		teste();
+		solucao();
+	}
+	static void solucao () {
+		Function<Produto, Double> precoFinal = produto -> {
+			return produto.preco * (1 - produto.desconto);
+		};
+		UnaryOperator<Double> impostoMunicipal = preco -> {
+			return preco >= 2500 ?  preco * 1.085 : preco;
+		};
+		UnaryOperator<Double> frete = preco -> {
+			return preco >= 3000 ? preco + 100 : preco + 50;
+		};
+		Function<Double, String> arredondar = preco -> {
+			return String.format("%.2f", preco);
+		};
+		UnaryOperator<String> formatar = preco -> "R$ " + preco;
+		var iPad = new Produto("iPad", 3235.89, 0.5);
+		var preco = precoFinal
+				.andThen(impostoMunicipal)
+				.andThen(frete)
+				.andThen(arredondar)
+				.andThen(formatar)
+				.apply(iPad);
+		System.out.println("O preço final é " + preco);
 	}
 	static void teste() {
 		Function<Produto, Double> preçoÁVista = produto -> {
 			return produto.preco*(1-produto.desconto);
 		};
-		UnaryOperator<Double> imposto = preco -> preco > 2500 ?  preco * 1.085 : preco;
-		UnaryOperator<Double> frete = preco -> preco >= 3000 ? preco + 100 : preco + 50;
+		UnaryOperator<Double> imposto = preco -> {
+			return preco > 2500 ?  preco * 1.085 : preco;
+		};
+		UnaryOperator<Double> frete = preco -> {
+			return preco >= 3000 ? preco + 100 : preco + 50;
+		};
 		var iPobre = new Produto("iPobre", 199, 0.2);
-//		var ipad = new Produto("iPad", 3235.89, 0.13);
-		var result = imposto
-				.compose(frete)
-				.apply(preçoÁVista.apply(iPobre));
-		
+		var result = preçoÁVista
+				.andThen(imposto)
+				.andThen(frete)
+				.apply(iPobre);	
 		System.out.printf("R$ %.2f\n", result);
 	}
 }
