0ed66b2287a44b99ef42f1d934a317507fc65cd1
Audioteca Ancap <danielpotoco@gmail.com>
2023-01-27 15:09:10 -0300

    visto video aula com solução do desafio

--- src/streams/DesafioMap.java
+++ src/streams/DesafioMap.java
@@ -7,15 +7,29 @@ import java.util.function.UnaryOperator;
 
 public class DesafioMap {
 	public static void main(String[] args) {
-		mySolution();
-	}
-	static void mySolution() {
 		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
 		/*
 		 * 1. Número para String binária... 6 -> "110"
 		 * 2. Reverter a String... "110" -> "011"
 		 * 3. Converter de volta para inteiro -> "011" -> 3
 		 * */
+//		mySolution(numbers);
+		solution(numbers);
+	}
+	static void solution(List<Integer> numbers) {
+		UnaryOperator<String> reverse = sentense -> {
+			return new StringBuilder(sentense).reverse().toString();
+		};
+		Function<String, Integer> binaryToInteger = sentense -> {
+			return Integer.parseInt(sentense, 2);
+		};
+		numbers.stream()
+			.map(Integer::toBinaryString)
+			.map(reverse)
+			.map(binaryToInteger)
+			.forEach(System.out::println);
+	}
+	static void mySolution(List<Integer> numbers) {
 		Function<Integer, String> toBinary = number -> {
 			var result = "";
 			do {
@@ -34,10 +48,23 @@ public class DesafioMap {
 			}
 			return result;
 		};
+//		numbers.stream()
+//			.forEach(number -> System.out.print(number + "\t"));
+//		System.out.println();
+//		numbers.stream()
+//			.map(toBinary)
+//			.forEach(number -> System.out.print(number + "\t"));
+//		System.out.println();
+//		numbers.stream()
+//			.map(toBinary)
+//			.map(reverse)
+//			.forEach(number -> System.out.print(number + "\t"));
+//		System.out.println();
 		numbers.stream()
 			.map(toBinary)
 			.map(reverse)
 			.map(toInteger)
-			.forEach(System.out::println);
+			.map(number -> number + " ")
+			.forEach(System.out::print);
 	}
 }
