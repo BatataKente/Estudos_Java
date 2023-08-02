0edbc2a919a193caf376717dd79f0a70a9304a6b
BatataKente <danielpotoco@gmail.com>
2023-08-01 22:05:48 -0300

    mais um exercício feito

--- src/exercices/java_exercices_OO/structured_logic/loops/Loops.java
+++ src/exercices/java_exercices_OO/structured_logic/loops/Loops.java
@@ -1,11 +1,14 @@
 package exercices.java_exercices_OO.structured_logic.loops;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.function.Consumer;
 
 public class Loops {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        _2(input);
+        _3(input);
         input.close();
     }
 //1) Fazer uma classe Ex1Primos para:
@@ -48,6 +51,39 @@ public class Loops {
 //    determinar se um número é permutação do outro ou não. Ex: 431 é permutação
 //    de 143, 42 é permutação de 204, 1211 é permutação de 1112, etc. O programa
 //    só deve aceitar números positivos.
+    private static void _3(Scanner input) {
+        do {
+            System.out.print("Informe o primeiro número: ");
+            var text_1 = input.next();
+            System.out.print("Informe o segundo número: ");
+            var text_2 = input.next();
+            try {
+                Integer.parseInt(text_1);
+                Integer.parseInt(text_2);
+                var firstPermutesSecond = "";
+                var secondPermutesFirst = "";
+
+                for (char character : text_1.toCharArray()) {
+                    if(!text_2.contains(String.valueOf(character))) {
+                        firstPermutesSecond = " não"; break;
+                    }
+                }
+                for (char character : text_2.toCharArray()) {
+                    if(!text_1.contains(String.valueOf(character))) {
+                        secondPermutesFirst = " não"; break;
+                    }
+                }
+                System.out.println("O primeiro número" + firstPermutesSecond + " permuta do segundo.");
+                System.out.println("O segundo número" + secondPermutesFirst + " permuta do primeiro.");
+                System.out.print("Deseja continuar?(S/N) ");
+                if(input.next().equalsIgnoreCase("n")) {
+                    break;
+                }
+            } catch(Exception exception) {
+                System.out.println("Voce informou algum valor inválido.");
+            }
+        } while(true);
+    }
 //4) Fazer um programa para medir os reflexos do usuário. O programa deve:
 //            • Mostrar a palavra “Agora!” após um tempo aleatório e um número,
 //    também aleatório
