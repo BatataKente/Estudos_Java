89b4e12589cb24931dbe7f4fa51e4e09087ecc28
FOURSYS\djunqueira <Batata>
2023-08-25 13:52:15 -0300

    concluido um modulo do curso com sucesso

---
+++ src/cursos/alura/jre_e_jdk/sintaxe_variaveis_e_fluxo/desafio_opcional/Fatorial.java
@@ -0,0 +1,9 @@
+package cursos.alura.jre_e_jdk.sintaxe_variaveis_e_fluxo.desafio_opcional;
+
+public class Fatorial {
+	public static void main(String[] args) {
+		int n = 10;
+		for (int i = n; i > 0; i--) n *= i;
+		System.out.println("O resultado de 10 fatorial é " + n);
+	}
+}
---
+++ src/cursos/alura/jre_e_jdk/sintaxe_variaveis_e_fluxo/testa_condicional/_2.java
@@ -0,0 +1,18 @@
+package cursos.alura.jre_e_jdk.sintaxe_variaveis_e_fluxo.testa_condicional;
+
+public class _2 {
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais:");
+		int idade = 20;
+		int quantidadeDePessoas = 3;
+		boolean acompanhado = quantidadeDePessoas >= 2;
+//		if (idade >= 18 || quantidadeDePessoas >= 2) {
+//		if (idade >= 18 && quantidadeDePessoas >= 2) {
+		if (idade >= 18 && acompanhado) {
+//			System.out.println("Você tem 18+.");
+			System.out.println("Seja bem vindo.");
+		} else {
+			System.out.println("Infelismente voce não pode.");
+		}
+	}
+}
