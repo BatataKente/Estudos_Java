e5838249c098ae1fd94f6730985b8cecb87009ee
FOURSYS\djunqueira <Batata>
2023-09-15 16:00:28 -0300

    ajustes

--- src/cursos/alura/java_util/testes/TesteSort4.java
+++ src/cursos/alura/java_util/testes/TesteSort4.java
@@ -44,22 +44,15 @@ public class TesteSort4 {
         lista.add(cc3);
         lista.add(cc4);
 		
-        lista.sort(
-        		new Comparator<Conta>() {
-			        public int compare(Conta c1, Conta c2) {
-//			            return Integer.compare(c1.getNumero(), c2.getNumero());
-			            return c1.getNumero() - c2.getNumero();
-                    }
-                }
-        );
-
-        Comparator<Conta> comp = new Comparator<Conta>() {
-                public int compare(Conta c1, Conta c2) {
-                    String nomeC1 = c1.getTitular().getNome();
-                    String nomeC2 = c2.getTitular().getNome();
-                    return nomeC1.compareTo(nomeC2);
-                }
-        };
+//        lista.sort(
+//        		(c1, c2) -> c1.getNumero() - c2.getNumero()
+//        );
+//
+//        Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+//	            String nomeC1 = c1.getTitular().getNome();
+//	            String nomeC2 = c2.getTitular().getNome();
+//	            return nomeC1.compareTo(nomeC2);
+//        };
 
         for (Conta conta : lista) {
                 System.out.println(conta + ", " + conta.getTitular().getNome());
