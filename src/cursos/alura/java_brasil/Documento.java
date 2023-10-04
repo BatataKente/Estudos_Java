65278fbbc10a96fcec08362462d5b2190cb55945
FOURSYS\djunqueira <Batata>
2023-10-04 16:38:31 -0300

    novas implementacoes

---
+++ src/cursos/alura/java_brasil/Documento.java
@@ -0,0 +1,19 @@
+package cursos.alura.java_brasil;
+
+import br.com.caelum.stella.validation.InvalidStateException;
+import br.com.caelum.stella.validation.Validator;
+
+public class Documento {
+	public static void validar(Validator<String> validator, final String value) {
+		validar(validator, validator.getClass().getSimpleName().replace("Validator", ""), value);
+	}
+	public static void validar(Validator<String> document, final String ofName, final String value) {
+		System.out.print(ofName);
+		try {
+			document.assertValid(value);
+			System.out.println(" VÁLIDO");
+		} catch (InvalidStateException e) {
+			System.out.println(" INVÁLIDO : " + e);
+		}
+	}
+}
